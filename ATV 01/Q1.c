901fc142b44171c0a497cda284d2da842f827158
danmaxy <danieljatoba034@hotmail.com>
2024-10-26 20:27:33 -0300

    up

--- ATV 01/Q1.c
+++ ATV 01/Q1.c
@@ -2,7 +2,15 @@
 
 int main() {
 
+int idade;
+double salario;
+printf("Digite a idade:");
+scanf("%d", &idade);
+printf("digite seu salario:");
+scanf("%lf", &salario);
 
+printf("sua idade: %d \n" ,idade);
+printf("seu salario: %2.lf" ,salario);
 
     
 }
